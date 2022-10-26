9d8ed916a00934b530e4134d4b4080910f37cf68
claudneysessa <claudneysartisessa@gmail.com>
2022-10-25 21:59:49 -0300

    Criando a classe createCourseService.ts

---
+++ src/createCourseService.ts
@@ -0,0 +1,20 @@
+
+interface Course {
+    name: string,
+    duration?: number,
+    educator: string
+}
+
+class CreateCourseService {
+    execute({ name, duration=8, educator }: Course) {
+        console.log(
+            name,
+            duration,
+            educator
+        );
+        
+        return { name, duration, educator };
+    }
+}
+
+export default new CreateCourseService();
